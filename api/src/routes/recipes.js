df12307e62bee67811774b0b683c1b77fd0ae3d6
FM9408 <medina.766@hotmail.com>
2025-02-23 18:40:26 -0600

    creacion de las funciones en el server para tags

--- api/src/routes/recipes.js
+++ api/src/routes/recipes.js
@@ -1,11 +1,12 @@
 const {Router} = require("express")
-const {getAllRecipes, queryRecipesSearch,queryRecipesByTagName } = require("../controllers/recipes")  
+const {getAllRecipes, queryRecipesSearch,queryRecipesByTagName,queryAllTags } = require("../controllers/recipes")  
 
 const recipesRouter = Router()
 
 recipesRouter.get("/all", getAllRecipes)
 recipesRouter.get("/search", queryRecipesSearch)
 recipesRouter.get("/searchByTag", queryRecipesByTagName)
+recipesRouter.get("/getTags",queryAllTags )
 
 
 
